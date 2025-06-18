7b7d057fa4a553b9171f76fce8e85e270489648e
Nana Fujita <nanaefuj@usc.edu>
2025-06-18 20:50:37 +0900

    removed build directory:

--- VisualAlgorithms/build/Qt_6_9_1_for_macOS-Debug/.qt/appVisualAlgorithms_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appVisualAlgorithms")
-set(working_dir "/Users/nanaefujita/Qt Projects/VisualAlgorithms")
-set(src_and_dest_list
-    "/Users/nanaefujita/Qt Projects/VisualAlgorithms/Main.qml"
-    "/Users/nanaefujita/Qt Projects/VisualAlgorithms/build/Qt_6_9_1_for_macOS-Debug/VisualAlgorithms/Main.qml"
-
-)
-set(timestamp_file "/Users/nanaefujita/Qt Projects/VisualAlgorithms/build/Qt_6_9_1_for_macOS-Debug/.qt/appVisualAlgorithms_qml.txt")
--- VisualAlgorithms/build/Qt_6_9_1_for_macOS-Debug/.qt/appVisualAlgorithms_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "appVisualAlgorithms")
-set(working_dir "/Users/nanaefujita/Qt Projects/VisualAlgorithms")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/Users/nanaefujita/Qt Projects/VisualAlgorithms/build/Qt_6_9_1_for_macOS-Debug/.qt/appVisualAlgorithms_res.txt")
