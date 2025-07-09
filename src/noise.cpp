f335a7714acd8126bb766260a82a0af369f019d3
Nana Fujita <nanaefuj@usc.edu>
2025-07-09 09:42:50 +0900

    implented preventing 0 period logic

--- src/noise.cpp
+++ src/noise.cpp
@@ -6,7 +6,7 @@ Noise::Noise(QObject *parent)
 
 void Noise::setPeriod(int period)
 {
-    period = std::abs(period);
+    period = (period > 0) ? std::abs(period) : 1;
     if (period != 0 && m_period != period) {
         updateLatticePeriod(period);
         m_period = period;
